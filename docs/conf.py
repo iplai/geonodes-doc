4a6047f94762cb3d6026153bc85df6beb29979a2
iplai <365141210@qq.com>
2023-03-18 04:16:22 +0800

    init

---
+++ docs/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = '程序化几何节点'
+copyright = '2023, 赛博念咒师'
+author = '赛博念咒师'
+release = '3.4.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx_copybutton",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ['_static']
+
+html_theme = 'furo'
+html_theme_options = {
+    "light_logo": "logo-light.png",
+    "dark_logo": "logo-dark.png",
+    "sidebar_hide_name": True,
+    "source_repository": "https://github.com/iplai/geonodes_bilibili",
+    "source_branch": "main",
+    "source_directory": "source/",
+}
+html_favicon = "_static/favicon-light.png"
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. geonodes_bilibili documentation master file, created by
+   sphinx-quickstart on Sat Mar 18 02:01:04 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+程序化几何节点说明文档
+=============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+索引与搜索
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
